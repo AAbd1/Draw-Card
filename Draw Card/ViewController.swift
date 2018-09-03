73e34dbdf8c93d46c4d85475f617cabe2c7d4712
Abdi <abdikadirpro@gmail.com>
2018-09-03 10:23:54 -0600

    first commit

---
+++ Draw Card/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Draw Card
+//
+//  Created by Abdi on 9/3/18.
+//  Copyright © 2018 Abdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
